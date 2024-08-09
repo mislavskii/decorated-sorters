5be17861ede95ba3a7e1f26c0fe324f9d7a0b9df
mislavskii <jornjat@gmail.com>
2024-08-09 21:07:10 +0700

    removed the faulty implementation of selection sorter

---
+++ main.py
@@ -0,0 +1,12 @@
+import sortfunc as sf
+import random
+
+n = 10000
+data = [random.randint(1, n * 2) for i in range(n)]
+sorters = [sf.bubble_sort, sf.selection_sort, sf.insertion_sort]
+random.shuffle(sorters)
+print([sorter.__name__ for sorter in sorters])
+
+if __name__ == '__main__':
+    print(f'data head(7)={data[:7]}, n={len(data)}')
+    [print(sf.print_execution_time(sorter, data)[:7]) for sorter in sorters]
---
+++ sortfunc.py
@@ -0,0 +1,42 @@
+def bubble_sort(listlike):
+    ls = listlike.copy()
+    for i in range(len(ls) - 1, 0, -1):
+        for j in range(i):
+            if ls[j] > ls[j + 1]:
+                ls[j], ls[j + 1] = ls[j + 1], ls[j]
+    return ls
+
+
+def selection_sort(listlike):
+    ls = listlike.copy()
+    for i in range(len(ls)):
+        min_index = i
+        for j in range(i, len(ls)):
+            if ls[min_index] > ls[j]:
+                min_index = j
+        if min_index != i:
+            ls[min_index], ls[i] = ls[i], ls[min_index]
+    return ls
+
+
+def insertion_sort(listlike):
+    ls = listlike.copy()
+    for i in range(1, len(ls)):
+        key = ls[i]
+        j = i - 1
+        while ls[j] > key and j >= 0:
+            ls[j + 1] = ls [j]
+            j -= 1
+        ls[j + 1] = key
+    return ls
+
+
+def print_execution_time(some_func, *args):
+    from time import time
+    print()
+    print(some_func.__name__, ':', sep='')
+    start = time()
+    res = some_func(*args)
+    finish = time()
+    print(f'executed in {round(finish - start, 3)} seconds')
+    return res
