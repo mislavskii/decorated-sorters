9b26908c82af176cd5a36e4d36d8dd6486ef95fc
mislavskii <jornjat@gmail.com>
2024-08-11 16:20:01 +0700

    added kwargs handling

--- sortfunc.py
+++ sortfunc.py
@@ -31,12 +31,12 @@ def insertion_sort(listlike):
     return ls
 
 
-def print_execution_time(some_func, *args):
+def print_execution_time(some_func, *args, **kwargs):
     from time import time
     print()
     print(some_func.__name__, ':', sep='')
     start = time()
-    res = some_func(*args)
+    res = some_func(*args, **kwargs)
     finish = time()
     print(f'executed in {round(finish - start, 3)} seconds')
     return res
